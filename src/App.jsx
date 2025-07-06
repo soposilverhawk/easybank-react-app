5fee32311be970e24cf503bdb638de362c9bda60
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-07-07 01:01:22 +0400

    add main section for unshared content of pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Header from "./components/Shared/Header/Header"
+import Header from "./components/Shared/Header/Header";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -13,14 +13,16 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/careers" element={<Careers />} />
-        <Route path="/invitation" element={<Invitation />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/invitation" element={<Invitation />} />
+        </Routes>
+      </main>
     </div>
   );
 }
